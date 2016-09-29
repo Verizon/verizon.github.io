4d476ef6ca03521e8dfbda27f1280dae5c980a9c
Alice Wu <alice.wu@one.verizon.com>
2016-09-28 17:03:48 -0700

    react setup

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     loaders: [
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
       {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
+      // {test : /\.jsx?/, include : APP_DIR, loader : 'babel'}
     ]
   },
   plugins: [
