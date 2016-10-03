92fde4943683792df8ed2c303d689d9e61222e5a
Alice Wu <alice.wu@one.verizon.com>
2016-10-03 14:01:51 -0700

    React now renders with data from API.  Links appear to work

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {render} from 'react-dom';
 import jquery from 'jquery';
 import bootstrap from './packages/bootstrap.min.css';
 global.jQuery = jquery;
-
 import OpenSourceNavbar from './js/open_source_navbar.js';
 import MainNavbar from './js/main_navbar.js';
 import HeaderContent from './js/header_content.js';
@@ -21,6 +20,46 @@ var App = React.createClass({
       </div>
     );
   }
-})
+});
 
 ReactDOM.render(<App />, document.getElementById('content'));
+
+var call = new XMLHttpRequest();
+var repos;
+var divs = [];
+call.open("GET", "https://api.github.com/orgs/verizon/repos",true);
+call.onload = function(){
+  repos = JSON.parse(call.responseText);
+  console.log(repos);
+  repos.forEach(function(e, i){
+    divs.push(
+      <div key={i} className="panel panel-default repo-unit">
+        <div className="panel-heading repo-unit">
+          <a href={e.homepage}>
+            <h2>{e.name}</h2>
+          </a>
+        </div>
+        <div className="panel-body repo-unit">
+          <p>{e.description}</p>
+        </div>
+        <div className="panel-footer repo-unit">
+        </div>
+      </div>
+    );
+  });
+  console.log(divs);
+  var RepoElement = React.createClass({
+    render: function(){
+      return (
+        <div className="content">
+          {divs}
+        </div>
+      );
+    }
+  });
+  ReactDOM.render(<RepoElement />, document.getElementById("open-source-content"));
+};
+
+document.addEventListener("DOMContentLoaded", function(event){
+  call.send();
+});
--- src/js/open_source_content.js
+++ src/js/open_source_content.js
@@ -1,25 +1,25 @@
 var React = require('react');
 import RepoUnit from './repo_unit.js';
 
-// function init(){
-//   jquery.ajax({
-//     url: "https://api.github.com/orgs/verizon/repos",
-//     success: function(result){
-//       result.forEach(function(e, i){
-//
-//       });
-//     }
-//   });
-// }
-// init();
-
 var OpenSourceContent = React.createClass({
+
   render: function(){
+    // var repos;
+    // var divs = [];
+    // var call = new XMLHttpRequest();
+    // call.open("GET", "https://api.github.com/orgs/verizon/repos",true);
+    // call.onload = function(){
+    //   repos = JSON.parse(call.responseText);
+    // };
+    // call.send();
+
+
+    // console.log(divs);
     return (
-      <div className="open-source-content">
-        <RepoUnit />
+      <div className="open-source-content" id="open-source-content">
       </div>
     );
+
   }
 });
 
