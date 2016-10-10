6cee213f6c3f6781920f481add5cf480dfb492ef
Timothy Perrett <timothy.m.perrett@one.verizon.com>
2016-10-10 15:56:53 -0700

    option 1 for header

--- src/js/header_content.js
+++ src/js/header_content.js
@@ -1,24 +1,28 @@
 var React = require('react');
-var $ = require ('jquery');
-var Trianglify = require('Trianglify');
+// var $ = require ('jquery');
+// var Trianglify = require('Trianglify');
 
 var HeaderContent = React.createClass({
   componentDidMount: function(){
-    var header = $('#content-head')
-    var pattern = new Trianglify({
-      width: window.screen.width | header.outerWidth(),
-      height: header.outerHeight()*1.2,
-      cell_size: 50,
-      seed: Math.random(),
-      x_colors: 'Reds'
-    }).png()
+    // var header = $('#content-head')
+    // var pattern = new Trianglify({
+    //   width: window.screen.width | header.outerWidth(),
+    //   height: header.outerHeight(),
+    //   cell_size: 40,
+    //   seed: Math.random(),
+    //   x_colors: [ "#4B3434", "#4C4C4E", "#98999B", "#F0F0F0", "#98999B","#4C4C4E","#4B3434" ]
+    // }).png()
 
-    header.css('background-image', 'url('+pattern+')');
+    // header.css('background-image', 'url('+pattern+')');
   },
   render: function(){
     return (
       <div id="content-head" className="content-head">
-        <h1>Hello.</h1>
+        <h1>Verizon Engineering.</h1>
+        <p>Giving back to the open-source community.</p>
+        <p>
+          <a href="#"><img src="https://img.shields.io/github/stars/badges/shields.svg?style=social&label=Star" /></a>
+        </p>
       </div>
     );
   }
