df1decb77df43016ecaa9eafbcfb504a1938b0ec
Timothy Perrett <timothy.m.perrett@one.verizon.com>
2016-10-10 17:03:01 -0700

    option 2

--- src/js/header_content.js
+++ src/js/header_content.js
@@ -1,19 +1,19 @@
 var React = require('react');
-// var $ = require ('jquery');
-// var Trianglify = require('Trianglify');
+var $ = require ('jquery');
+var Trianglify = require('Trianglify');
 
 var HeaderContent = React.createClass({
   componentDidMount: function(){
-    // var header = $('#content-head')
-    // var pattern = new Trianglify({
-    //   width: window.screen.width | header.outerWidth(),
-    //   height: header.outerHeight(),
-    //   cell_size: 40,
-    //   seed: Math.random(),
-    //   x_colors: [ "#4B3434", "#4C4C4E", "#98999B", "#F0F0F0", "#98999B","#4C4C4E","#4B3434" ]
-    // }).png()
+    var header = $('#content-head')
+    var pattern = new Trianglify({
+      width: window.screen.width | header.outerWidth(),
+      height: header.outerHeight(),
+      cell_size: 40,
+      seed: Math.random(),
+      x_colors: 'Spectral'
+    }).png()
 
-    // header.css('background-image', 'url('+pattern+')');
+    header.css('background-image', 'url('+pattern+')');
   },
   render: function(){
     return (
@@ -21,8 +21,12 @@ var HeaderContent = React.createClass({
         <h1>Verizon Engineering.</h1>
         <p>Giving back to the open-source community.</p>
         <p>
-          <a href="#"><img src="https://img.shields.io/github/stars/badges/shields.svg?style=social&label=Star" /></a>
+          <small>
+            <span className="octicon octicon-globe"></span>
+            verizon.com
+          </small>
         </p>
+        <a className="arrow-down octicon octicon-chevron-down" href="#open-source-content"></a>
       </div>
     );
   }
