a609ea1049ccbb68c841697f24196984beaa3094
Scott-White, Tatiana Terese <tatiana.scott-white@one.verizon.com>
2020-07-16 18:00:14 -0400

    fix errors

--- verizon-os/src/components/calltoaction.js
+++ verizon-os/src/components/calltoaction.js
@@ -2,43 +2,37 @@ import React from 'react';
 import { Title, Subtitle } from '@vds/typography';
 
 const actionList = [
-      { 
-        "heading": "Use",
-        "url": "placeholder",
-        "text": "our open source code"
-      }, 
-      {
-        "heading": "Contribute",
-        "url": "placeholder",
-        "text": "to our projects"
-      },
-      {
-        "heading": "Join",
-        "url": "placeholder",
-        "text" : "our communities"
-      }
+  { 
+    "heading": "Use",
+    "url": "placeholder",
+    "text": "our open source code"
+  }, 
+  {
+    "heading": "Contribute",
+    "url": "placeholder",
+    "text": "to our projects"
+  },
+  {
+    "heading": "Join",
+    "url": "placeholder",
+    "text" : "our communities"
+  }
 ];
 
 const CallToAction = () => {
 
-  const actions = actionList.map((action) => {
-    return  <section className="card">
-              <article className="card-center">
-                <div className="card-body">
-                  <a href={action.url}>
-                    <Title>{action.heading}</Title>
-                  </a>
-                  <p>
-                    <Subtitle>{action.text}</Subtitle>
-                  </p>
-                </div>
-              </article>
-            </section>            
+  const actions = actionList.map((action, index) => {
+    return   <div className="card card-center card-body" key={index}>
+                <a href={action.url}>
+                  <Title>{action.heading}</Title>
+                </a>  
+                <Subtitle>{action.text}</Subtitle>
+              </div> 
   })
   return (
-      <div className="row">
-        {actions}
-      </div>
+    <div className="row">
+      {actions}
+    </div>
   )
 }
 
