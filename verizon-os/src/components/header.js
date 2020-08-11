d59fe1167c3a4f835c8b273cc59b846fbe2cd813
Scott-White, Tatiana Terese <tatiana.scott-white@one.verizon.com>
2020-08-11 16:37:07 -0400

    add semicolons and refactor indentation

--- verizon-os/src/components/header.js
+++ verizon-os/src/components/header.js
@@ -4,30 +4,30 @@ import logo from "../images/vz_150_rgb_p.jpg";
 import { Title, Body } from '@vds/typography';
 
 const Header = () => (
-    <header>
-        <div className="container">
-            <div className="inner-header">
-                <div className="logo">
-                    <img src={logo} alt="VzLogo"></img>
-                </div>
-                <div className="title">
-                    <Title size="medium">Verizon Open Source</Title>
-                </div>
-                <div className="navigation">
-                    <nav>
-                        <Link to="/" style={{ textDecoration: 'none' }}><Body size="large">Home</Body></Link>
-
-                        {/* Deprecating Blog Tab Until We Have A Blog Ready */}
-
-                        {/* <Link to="/blog"><Body size="large">Blog</Body></Link> */}
-                        
-                        <Link to="/projects" style={{ textDecoration: 'none' }}><Body size="large">Projects</Body></Link>
-                        <Link to="https://www.verizon.com/support/residential/internet/equipment/open-source-software" style={{ textDecoration: 'none' }}><Body size="large">Attributions</Body></Link>
-                    </nav>
-                </div>
-            </div>  
+  <header>
+    <div className="container">
+      <div className="inner-header">
+        <div className="logo">
+            <img src={logo} alt="VzLogo"></img>
         </div>
-    </header>
-)
+        <div className="title">
+            <Title size="medium">Verizon Open Source</Title>
+        </div>
+        <div className="navigation">
+          <nav>
+            <Link to="/" style={{ textDecoration: 'none' }}><Body size="large">Home</Body></Link>
+
+            {/* Deprecating Blog Tab Until We Have A Blog Ready */}
+
+            {/* <Link to="/blog"><Body size="large">Blog</Body></Link> */}
+            
+            <Link to="/projects" style={{ textDecoration: 'none' }}><Body size="large">Projects</Body></Link>
+            <Link to="https://www.verizon.com/support/residential/internet/equipment/open-source-software" style={{ textDecoration: 'none' }}><Body size="large">Attributions</Body></Link>
+          </nav>
+        </div>
+      </div>  
+    </div>
+  </header>
+);
 
 export default Header;
