70af80f3cec836898f0e427c0baa0e57b0b3ca40
Scott-White, Tatiana Terese <tatiana.scott-white@one.verizon.com>
2020-08-11 16:35:43 -0400

    remove underline from header links

--- verizon-os/src/components/header.js
+++ verizon-os/src/components/header.js
@@ -1,6 +1,6 @@
-import { Link } from "gatsby"
-import React from "react"
-import logo from "../images/vz_150_rgb_p.jpg"
+import { Link } from "gatsby";
+import React from "react";
+import logo from "../images/vz_150_rgb_p.jpg";
 import { Title, Body } from '@vds/typography';
 
 const Header = () => (
@@ -11,26 +11,23 @@ const Header = () => (
                     <img src={logo} alt="VzLogo"></img>
                 </div>
                 <div className="title">
-                    <Title size="large">Verizon Open Source</Title>
+                    <Title size="medium">Verizon Open Source</Title>
                 </div>
                 <div className="navigation">
                     <nav>
-                        <Link to="/"><Body size="large">Home</Body></Link>
+                        <Link to="/" style={{ textDecoration: 'none' }}><Body size="large">Home</Body></Link>
 
                         {/* Deprecating Blog Tab Until We Have A Blog Ready */}
 
                         {/* <Link to="/blog"><Body size="large">Blog</Body></Link> */}
                         
-                        <Link to="/projects"><Body size="large">Projects</Body></Link>
-                        <Link to="https://www.verizon.com/support/residential/internet/equipment/open-source-software"><Body size="large">Attributions</Body></Link>
+                        <Link to="/projects" style={{ textDecoration: 'none' }}><Body size="large">Projects</Body></Link>
+                        <Link to="https://www.verizon.com/support/residential/internet/equipment/open-source-software" style={{ textDecoration: 'none' }}><Body size="large">Attributions</Body></Link>
                     </nav>
                 </div>
-                
-            </div>
-                
+            </div>  
         </div>
-
     </header>
 )
 
-export default Header
+export default Header;
