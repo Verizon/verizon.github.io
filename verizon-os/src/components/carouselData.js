fe97d084267e06d2a2501dd7031a3024e3e3d35d
v676481 <vamsi.ponnapalli@verizon.com>
2020-08-21 14:54:38 -0400

    update projects in carousel

--- verizon-os/src/components/carouselData.js
+++ verizon-os/src/components/carouselData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "gatsby";
 import os1 from "../images/open-source-1.jpg";
 import os2 from "../images/open-source-2.jpg";
 import os3 from "../images/open-source-3.jpg"; 
@@ -7,7 +8,6 @@ import { Body, Title } from '@vds/typography';
 import { Button } from '@vds/buttons';
 
 class Slide1 extends Component {
-    handleClickEvent = () => alert('You clicked the Button example!');
     render() {
       return (<div className="slideData">
         <img src={ os1 } alt="os1"></img>
@@ -17,14 +17,13 @@ class Slide1 extends Component {
             <Title size="large" color="#747676">But we’re coming around to it.</Title>
         </div>
         <div className="slideButton1">
-            <Button onClick={this.handleClickEvent}>Learn More</Button>
+            <Link to="/community"><Button>Learn More</Button></Link>
         </div>
       </div>);
     };
 }
 
 class Slide2 extends Component {
-    handleClickEvent = () => alert('You clicked the Button example!');
     render() {
       return (<div className="slideData">
         <img src={ os2 } alt="os2"></img>
@@ -34,37 +33,39 @@ class Slide2 extends Component {
             <Body size="large" bold={true} color="#333333">Thread-safe. Mutation-safe.</Body>
         </div>
         <div className="slideButton2">
-            <Button onClick={this.handleClickEvent}>Project Details</Button>
+            <Link to="https://github.com/Verizon/safecache" target="_blank"><Button>Project Details</Button></Link>
         </div>
       </div>);
     };
   }
 
 class Slide3 extends Component {
-    handleClickEvent = () => alert('You clicked the Button example!');
     render() {
       return (<div className="slideData">
         <img src={ os3 } alt="os3"></img>
         <div className="slideTitle2">
-            <Title size="large" color="#747676"> Project Project</Title>
+            <Title size="large" color="#747676">YANG-validator</Title>
+            <br></br>
+            <Body size="large" bold={true} color="#333333">A project to help equipment vendors validate against OpenConfig YANG</Body>
         </div>
         <div className="slideButton2">
-            <Button onClick={this.handleClickEvent}>Check Me Out</Button>
+            <Link to="https://github.com/Verizon/YANG-validator" target="_blank"><Button>Project Details</Button></Link>
         </div>
       </div>);
     };
   }
 
 class Slide4 extends Component {
-    handleClickEvent = () => alert('You clicked the Button example!');
     render() {
       return (<div className="slideData">
         <img src={ os4 } alt="os4"></img>
         <div className="slideTitle2">
-            <Title size="large" color="#333333"> Project Project</Title>
+            <Title size="large" color="#333333">YANG-transformer</Title>
+            <br></br>
+            <Body size="large" bold={true} color="#333333">Transformer library for OpenConfig YANG JSON</Body>
         </div>
         <div className="slideButton2">
-            <Button onClick={this.handleClickEvent}>Check Me Out</Button>
+        <Link to="https://github.com/Verizon/YANG-transformer" target="_blank"><Button>Project Details</Button></Link>
         </div>
       </div>);
     };
