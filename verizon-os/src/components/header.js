53dc8754f32fed1913999be26ed6e2174e089a69
Scott-White, Tatiana Terese <tatiana.scott-white@one.verizon.com>
2020-09-04 10:33:34 -0400

    fix attributes link

--- verizon-os/src/components/header.js
+++ verizon-os/src/components/header.js
@@ -26,7 +26,7 @@ const Header = () => (
             
             <Link to="/projects" style={{ textDecoration: 'none' }}><Body size="medium" bold={true}>Projects</Body></Link>
             <Link to="/community" style={{ textDecoration: 'none' }}><Body size="medium" bold={true}>Community</Body></Link>
-            <Link to="/attributions" style={{ textDecoration: 'none' }}><Body size="medium" bold={true}>Attributions</Body></Link>
+            <a href="https://www.verizon.com/support/residential/internet/equipment/open-source-software" style={{ textDecoration: 'none' }} target="_blank" rel="noreferrer"><Body size="medium" bold={true}>Attributions</Body></a>
           </nav>
         </div>
       </div>  
--- verizon-os/src/pages/index.js
+++ verizon-os/src/pages/index.js
@@ -7,7 +7,6 @@ import OsPage from '../pages/community';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Projects from './projects'; 
-import Attributions from './attributions';
 
 
 const IndexPage = () => (
@@ -18,7 +17,6 @@ const IndexPage = () => (
                 <Route path="/home" component={HomePage} />
                 {/* <Route path="/blog" component={BlogPage} /> */}
                 <Route path="/projects" component={Projects} />
-                <Route path="/attributions" component={Attributions}/>
                 <Route path="/community" component={OsPage}/>
             </Switch>
         <Footer/>
