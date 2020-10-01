2e4119722d18bbc8683817745163cf58cd17dca9
Scott-White, Tatiana Terese <tatiana.scott-white@one.verizon.com>
2020-10-01 16:43:26 -0400

    remove extra project card space

--- verizon-os/src/components/header.js
+++ verizon-os/src/components/header.js
@@ -8,14 +8,11 @@ const Header = () => (
     <div className="container">
       <div className="inner-header">
         <div className="logo">
-            <img src={logo} alt="VzLogo"></img>
-            <div className="opensource-text">
-              <Title size="large">Open Source</Title>
-            </div>
+          <img src={logo} alt="VzLogo"></img> 
         </div>
-        {/* <div className="title">
-            <Title size="small">Open Source</Title>
-        </div> */}
+        <div className="opensource-text">
+          <Title size="large">Open Source</Title>
+        </div>
         <div className="navigation">
           <nav>
             <Link to="/home" style={{ textDecoration: 'none' }}><Body size="medium" bold={true}>Home</Body></Link>
