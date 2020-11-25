f824fe0c800befc742ca5a5a1017226332a1ca16
v676481 <vamsi.ponnapalli@verizon.com>
2020-11-25 10:26:02 -0500

    reverting until we figure out access token issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 
 module.exports = {
   /* Your site config here */
