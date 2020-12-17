b0059c545cf1d79a0e196bf458b225a8a04822da
v676481 <vamsi.ponnapalli@verizon.com>
2020-12-17 11:23:16 -0500

    still need to fix 404 page

--- src/404.js
+++ src/404.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Title, Subtitle } from '@vds/typography'; 
 
 const NotFoundPage = () => {
     return (
-    <div>
-        <p>404</p>
-    </div>
+        <div className="notFound">
+            <Title>404</Title>
+            <Subtitle>Uh oh! I think you've reached a bad URL!</Subtitle>
+        </div>
     )
 }
 
