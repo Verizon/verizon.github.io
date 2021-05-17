0cdb3f0f5980a499413d2caa2b43836071e97a3b
v676481 <vamsi.ponnapalli@verizon.com>
2021-05-17 09:17:54 -0400

    adjusting order of slides

--- src/components/carouselData.js
+++ src/components/carouselData.js
@@ -27,8 +27,25 @@ class Slide1 extends Component {
       </div>);
     }
 }
-
 class Slide2 extends Component {
+  render() {
+    return (<div className="slideData">
+      <img src={ os4 } alt="os4"></img>
+      <div className="slide">
+        <div className="slideTitle">
+          <Title size="large" viewport="mobile" color="#000000">5G Edge Tutorials</Title>
+          <br></br>
+          <Title size="small" viewport="mobile" bold={true} color="#000000">Launch your first application at the network edge.</Title>
+        </div>
+        <div className="slideButton">
+          <a href="https://github.com/Verizon/5GEdgeTutorials" target="_blank" rel="noreferrer"><Button viewport="mobile">Project Info</Button></a>
+        </div>
+      </div>
+    </div>);
+  }
+}
+
+class Slide3 extends Component {
   render() {
     return (<div className="slideData">
       <img src={ os2 } alt="os2"></img>
@@ -46,7 +63,7 @@ class Slide2 extends Component {
   }
 }
 
-class Slide3 extends Component {
+class Slide4 extends Component {
     render() {
       return (<div className="slideData">
         <img src={ os3 } alt="os3"></img>
@@ -65,22 +82,5 @@ class Slide3 extends Component {
   }
 
 
-class Slide4 extends Component {
-    render() {
-      return (<div className="slideData">
-        <img src={ os4 } alt="os4"></img>
-        <div className="slide">
-          <div className="slideTitle">
-            <Title size="large" viewport="mobile" color="#000000">5G Edge Tutorials</Title>
-            <br></br>
-            <Title size="small" viewport="mobile" bold={true} color="#000000">Launch your first application at the network edge.</Title>
-          </div>
-          <div className="slideButton">
-            <a href="https://github.com/Verizon/5GEdgeTutorials" target="_blank" rel="noreferrer"><Button viewport="mobile">Project Info</Button></a>
-          </div>
-        </div>
-      </div>);
-    }
-  }
   
   export { Slide1, Slide2, Slide3, Slide4}
